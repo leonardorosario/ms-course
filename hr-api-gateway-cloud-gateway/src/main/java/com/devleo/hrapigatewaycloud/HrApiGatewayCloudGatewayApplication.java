31f3c47dfb5d89c3a55df874fb7843b11131290a
Leonardo Rosario <leobr4417@gmail.com>
2026-01-23 19:14:18 -0300

    Api gateway cloud created

---
+++ hr-api-gateway-cloud-gateway/src/main/java/com/devleo/hrapigatewaycloud/HrApiGatewayCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.devleo.hrapigatewaycloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrApiGatewayCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrApiGatewayCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway-cloud-gateway/src/test/java/com/devleo/hrapigatewaycloud/HrApiGatewayCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devleo.hrapigatewaycloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
